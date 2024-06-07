9f048765bbe7f91833ff2d885091233b429f411c
cotpetru24 <cotpetru1@gmail.com>
2024-06-07 11:58:01 +0100

    cleaned the functions.js file

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,3 +1,4 @@
+// Function to set recipe rating stars
 export function formatDishRating(rating) {
     const yellowStar = '<img class="ratingStars" src="/public/img/icons8-star-filled-30-yellow.png" alt="Yellow Star" />';
     const grayStar = '<img class="ratingStars" src="/public/img/icons8-star-filled-30-gray.png" alt="Gray Star" />';
@@ -5,6 +6,7 @@ export function formatDishRating(rating) {
 }
 
 
+// Function to set a string to title case
 export function toTitleCase(str) {
     if (typeof str !== 'string') {
         return '';
@@ -15,12 +17,14 @@ export function toTitleCase(str) {
 }
 
 
-
+// Function to format time and icon for recipe prep. time
 export function formatPrepTime(prepTime) {
     if (prepTime / 60 < 1) return `&#x1F552; ${prepTime} min`;
     else return `&#x1F552; ${Math.floor(prepTime / 60)} h ${prepTime % 60} min.`
 }
 
+
+// Function to convert a string to JSON array
 export function convertToJSONArray(string, delimiter = ','){
     let array = string.split(delimiter).map(item=>item.trim());
     let jsonArray = JSON.stringify(array);
@@ -34,6 +38,7 @@ export function convertToJSONArray(string, delimiter = ','){
 // }
 
 
+// Functon to change the CSS class messages flag
 export function toggleFlagClass(flagButton) {
     if (flagButton.classList.contains('flag-pink')) {
         flagButton.classList.remove('flag-pink');
@@ -44,6 +49,8 @@ export function toggleFlagClass(flagButton) {
     }
 }
 
+
+// Function to convert an image to base64 (for storring in DB)
 export function convertImageToBase64(imageFile) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
